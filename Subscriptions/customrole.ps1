d059fa09a2e19c07af5d3d9a464ab08cecb9ceb0
Ajay Pathak <APathak2@slb.com>
2019-08-06 09:07:54 +0530

    Create a custom role to restart and start virtual machine

--- Subscriptions/customrole.ps1
+++ Subscriptions/customrole.ps1
@@ -11,4 +11,33 @@ $role=Get-AzRoleDefinition -Name "Virtual Machine Contributor"
 $role| ConvertTo-Json | Out-File .\Subscriptions\"json\Virtual Machine Contributor.json"
 $role.Actions
 
-Get-AzProviderOperation "Microsoft.Compute/virtualMachines/*" | FT OperationName, Operation, Description -AutoSize | Out-File .\Subscriptions\json\"virtualmachineoperations.json"
+Get-AzProviderOperation "Microsoft.Compute/virtualMachines/*" | `
+ FT OperationName, Operation, Description -AutoSize | `
+  Out-File .\Subscriptions\json\"virtualmachineoperations.json"
+
+  $subscription=Get-AzSubscription -SubscriptionName "Visual Studio Enterprise" 
+  $subscription
+
+  
+ $customRoleName="Virtual Machine Operator"
+ $customRole=[Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
+ $customRole.Name=$customRoleName
+ $customRole.Id=$null
+ $customRole.Description = "Can monitor and restart virtual machines."
+ $customRole.IsCustom=$true
+ $perms = 'Microsoft.Storage/*/read','Microsoft.Network/*/read','Microsoft.Compute/*/read'
+ $perms += 'Microsoft.Compute/virtualMachines/start/action','Microsoft.Compute/virtualMachines/restart/action'
+ $perms += 'Microsoft.Authorization/*/read'
+ $perms += 'Microsoft.ResourceHealth/availabilityStatuses/read'
+ $perms += 'Microsoft.Resources/subscriptions/resourceGroups/read'
+ $perms += 'Microsoft.Insights/alertRules/*','Microsoft.Support/*'
+ $customRole.Actions=$perms
+ $customRole.NotActions = (Get-AzRoleDefinition -Name 'Virtual Machine Contributor').NotActions
+ 
+ $customRole.AssignableScopes="/subscriptions/"+$subscription.SubscriptionId
+ 
+ New-AzRoleDefinition -Role $customRole
+Get-AzRoleDefinition -Name $customRoleName | ConvertTo-Json | Out-File '.\Subscriptions\json\Virtual Machine Operator.json'
+
+
+ 
